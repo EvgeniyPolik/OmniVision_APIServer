c87f470339c49fb5740a4e33cacfca0aa9326e56
EvgeniyPolik <98385225+EvgeniyPolik@users.noreply.github.com>
2022-05-14 22:36:52 +0500

    Организован опрос приборов. Организован поток для обновления списка и постоянного опроса

--- OmniVision/OmniVision_APIserver/Boller.cs
+++ OmniVision/OmniVision_APIserver/Boller.cs
@@ -6,28 +6,32 @@ public class Boller
     public int Id;
     public string[] Properties = new string[4];
     public string Ip;
+    public int ShemaControl;
 
-    public Boller(int ids, string name, string city, string street, string bulding, string ips)
+    public Boller(int ids, string name, string city, string street, string bulding, string ips, int shema)
     {
         Id = ids;
         Properties[0] = name;
-        Properties[0] = city;
-        Properties[1] = street;
-        Properties[2] = bulding;
+        Properties[1] = city;
+        Properties[2] = street;
+        Properties[3] = bulding;
         Ip = ips;
+        ShemaControl = shema;
     }
     public Boller()
     {
         Id = -1;
-        Properties[0] = "City";
-        Properties[1] = "Street";
-        Properties[2] = "Bulding";
+        Properties[0] = "name";
+        Properties[1] = "City";
+        Properties[2] = "Street";
+        Properties[3] = "Bulding";
         Ip = "0.0.0.0";
+        ShemaControl = 0;
     }
 
     public override string ToString()
     {
-        return string.Format("Index: {0}, City: {1}, Street: {2}, Build: {3}, ip-adress: {4}", 
-            Id, Properties[0], Properties[1], Properties[2], Ip);
+        return string.Format("Index: {0}, Name: {1}, City: {2}, Street: {3}, Build: {4}, ip-adress: {5}, NumberShema: {6}", 
+            Id, Properties[0], Properties[1], Properties[2], Properties[3], Ip, ShemaControl);
     }
 }
--- OmniVision/OmniVision_APIserver/Program.cs
+++ OmniVision/OmniVision_APIserver/Program.cs
@@ -4,10 +4,15 @@
 //Установить FirebirdSQL.EntityFrameworkCore.Firebird
 //Установить System.Text.Encoding.CodePage + добавить зависимость от соответсвующей длл 
 //Установить Newtonsoft.Json 
-//Использована длл easymodbus.dll установить зависимость
+//Использована длл FluentModbus установить зависимость
+
+using System.Net;
+using System.Net.Sockets;
+using NModbus;
+using NModbus.Utility;
 using System.Text;
 using System.Threading;
-using Modbus;
+
 using FirebirdSql.Data.FirebirdClient;
 using Newtonsoft.Json;
 
@@ -20,27 +25,45 @@ namespace OmniVision_APIserver
 
         public static void UpdateCatalog()
         {
+            int countRepit = 120;
             while (true)
             {
-                ListOfBollers = MakeNewCatalog();
-                Console.WriteLine("Update" + DateTime.Now);
-                Thread.Sleep(900 * 1000);
+                if (countRepit > 119)
+                {
+                    ListOfBollers = MakeNewCatalog();
+                    Console.WriteLine("Update catalog: " + DateTime.Now);
+                    UpdHealth();
+                    countRepit = 0;
+                    Thread.Sleep(15 * 1000);
+                }
+                else
+                {
+                    UpdHealth();
+                    countRepit++;
+                    Thread.Sleep(15 * 1000);
+                }
+
             }
         }
         
          public static void UpdHealth()
          {
-             Console.WriteLine("2");
              HealthBoller.Clear();
-             bool[] status = new bool[4];
+             Console.WriteLine($"New helth information on {DateTime.Now}: ");
              for (int i = 0; i < ListOfBollers.Count; i++)
              {
-                 Console.WriteLine(ListOfBollers[i].Ip);
-                 Mo kontroller = new ModbusClient(ListOfBollers[i].Ip, 502);
-        //         kontroller.Connect();
-        //         status = kontroller.ReadCoils(8192, 4);
-        //         HealthBoller[ListOfBollers[i].Id] = status;
-        //         Console.WriteLine(JsonConvert.SerializeObject(HealthBoller[ListOfBollers[i].Id]));
+                 TcpClient clientTCP = new TcpClient(ListOfBollers[i].Ip, 502);
+                 var targetKontroller = new ModbusFactory();
+                 IModbusMaster modbusServer = targetKontroller.CreateMaster(clientTCP);
+                 bool[] dq = modbusServer.ReadCoils(0, 8192, 4);  // Discrete outputs
+                 ushort[] ai = modbusServer.ReadInputRegisters(0, 0, 2);  // Analog inputs
+                 bool[] di = modbusServer.ReadInputs(0, 0, 6); // Discrete inputs
+                 bool[] flag = modbusServer.ReadCoils(0, 8258, 1); // Read flag
+                 Console.WriteLine($"Q1:{dq[0]}, Q2:{dq[1]}, Q3:{dq[2]}, Q4:{dq[3]}"); 
+                 Console.WriteLine($"A1:{ai[0]}, A2:{ai[1]}"); 
+                 Console.WriteLine($"I1:{di[0]}, I2:{di[1]}, I3:{di[2]}, I4:{di[3]}, I5:{di[4]}, I6:{di[5]}");
+                 Console.WriteLine($"Flag M3:{flag[0]}");
+
              }
          }
 
@@ -49,7 +72,7 @@ namespace OmniVision_APIserver
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance); // Для подключения кодировки win1251
             FbConnectionStringBuilder fbConn = new FbConnectionStringBuilder(); // Переменная с параметрами подключения к БД
             fbConn.DataSource = "127.0.0.1"; // Записываем в переменную параметры
-            fbConn.Database = @"C:\C#\OmniVision\MBD.fdb";
+            fbConn.Database = @"D:\Rider\education C#\OmniVision\MBD.fdb";
             fbConn.UserID = "SYSDBA";
             fbConn.Password = "masterkey";
             fbConn.Charset = "WIN1251";
@@ -57,7 +80,7 @@ namespace OmniVision_APIserver
             FbConnection dbConn = new FbConnection(fbConn.ToString()); // Создадим коннект к БД
             dbConn.Open(); // Активируем коннект
             FbTransaction fbt = dbConn.BeginTransaction(); //  Создадим транзакцию
-            FbCommand selectSql = new FbCommand("SELECT ID_K, NAM_K, TIP_NP, NAM_NP, TIP_U, NAM_U, K_DOM, IP_ADR " +
+            FbCommand selectSql = new FbCommand("SELECT ID_K, NAM_K, TIP_NP, NAM_NP, TIP_U, NAM_U, K_DOM, IP_ADR, SHEMA_K " +
                                                 "FROM KOT LEFT JOIN NP ON K_NP = ID_NP LEFT JOIN ULC ON ID_U = K_U WHERE " +
                                                 "K_DEL = 0 AND SHEMA_K > 0", dbConn);
             selectSql.Transaction = fbt; // Инициализация запроса транзакцией
@@ -66,11 +89,10 @@ namespace OmniVision_APIserver
             while (reader.Read()) // Запишем результат в переменную для отправки
             {
                 ListOfBollers.Add(new Boller(reader.GetInt32(0), reader.GetString(1),
-                    reader.GetString(2) + " " + reader.GetString(3), reader.GetString(4) + " " +
-                                                                     reader.GetString(5), reader.GetString(6),
-                    reader.GetString(7)));
+                    reader.GetString(2) + " " + reader.GetString(3), reader.GetString(4) + " " + 
+                    reader.GetString(5), reader.GetString(6), reader.GetString(7), reader.GetInt32(8)));
             }
-
+            
             reader.Close(); // Обязательно закрываем запрос
             dbConn.Close();
             return ListOfBollers;
@@ -102,15 +124,11 @@ namespace OmniVision_APIserver
             app.UseAuthorization();
 
             app.MapControllers();
-            
-            MakeNewCatalog();
-            UpdHealth();
-            
+         
 // Организация отдельного потока для создания списка котельных
             Thread updatingCatalog = new Thread(new ThreadStart(UpdateCatalog));
             updatingCatalog.Start();
-
-            
+           
             app.Run();
         }
     }
